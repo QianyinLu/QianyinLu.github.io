da03788b403b3d6f5aade1d08089358340a793d7
QianyinLu <qianyin.lu@duke.edu>
2020-09-22 11:41:24 -0400

    hw 3

---
+++ content/plot.md
@@ -0,0 +1,11 @@
+Title: Project Malaria Plot
+Category: Plot 
+Date: 2020-09-22 10:01
+Modified: 2020-08-24 12:30
+Tags: plot, python
+Slug: plot
+Authors: Presnie Lu
+Summary: Plot in python
+
+The first figure:  
+![my_image](/img/plot1.png "Image Title on mouse-over")
--- pelicanconf.py
+++ pelicanconf.py
@@ -40,3 +40,5 @@ THEME = "/Users/presnie/attila"
 HOME_COVER = "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
 
 MD_EXTENSIONS = ['codehilite(noclasses=True, pygments_style=native)', 'extra'] 
+
+STATIC_PATHS = ['img']
